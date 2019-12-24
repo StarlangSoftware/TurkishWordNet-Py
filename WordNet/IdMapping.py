a2e009e5593e94479fc0acff07ef5d793d015941
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-12-24 18:36:45 +0300

    Updated WordNet repository translation

---
+++ WordNet/IdMapping.py
@@ -0,0 +1,105 @@
+class IdMapping:
+
+    __map: dict = {}
+
+    """
+    Constructor to load ID mappings from given file to a map.
+
+    PARAMETERS
+    ----------
+    fileName : str
+        String file name input that will be read
+    """
+    def __init__(self, fileName=None):
+        if fileName is not None:
+            infile = open(fileName, "r")
+            lines = infile.readlines()
+            for line in lines:
+                items = line.split("->")
+                self.__map[items[0]] = items[1]
+
+    """
+    Returns a Set view of the keys contained in this map.
+
+    RETURNS
+    -------
+    set
+        A set view of the keys contained in this map
+    """
+    def keySet(self) -> set:
+        return set(self.__map.keys())
+
+    """
+    Returns the value to which the specified key is mapped, or None if this map contains no mapping for the key.
+
+    PARAMETERS
+    ----------
+    id : str
+        String id of a key
+        
+    RETURNS
+    -------
+    str
+        Value of the specified key
+    """
+    def map(self, id: str) -> str:
+        if id not in self.__map:
+            return None
+        mappedId = self.__map[id]
+        while mappedId in self.__map:
+            mappedId = self.__map[mappedId]
+        return mappedId
+
+    """
+    Returns the value to which the specified key is mapped.
+
+    PARAMETERS
+    ----------
+    id : str
+        String id of a key
+        
+    RETURNS
+    -------
+    str
+        Value of the specified key
+    """
+    def singleMap(self, id: str) -> str:
+        return self.__map[id]
+
+    """
+    Associates the specified value with the specified key in this map.
+
+    PARAMETERS
+    ----------
+    key : str  
+        key with which the specified value is to be associated
+    value : str
+        value to be associated with the specified key
+    """
+    def add(self, key: str, value: str):
+        self.__map[key] = value
+
+    """
+    Removes the mapping for the specified key from this map if present.
+
+    PARAMETERS
+    ----------
+    key : str
+        key whose mapping is to be removed from the map
+    """
+    def remove(self, key: str):
+        self.__map.pop(key)
+
+    """
+    Saves map to the specified file.
+
+    PARAMETERS
+    ----------
+    fileName : str
+        String file to write map
+    """
+    def save(self, fileName: str):
+        outfile = open(fileName, "w")
+        for key in self.__map:
+            outfile.write(key + "->" + self.__map[key] + "\n")
+        outfile.close()
---
+++ WordNet/Synonym.py
@@ -0,0 +1,143 @@
+from WordNet.Literal import Literal
+
+
+class Synonym:
+
+    __literals: list = []
+
+    """
+    A constructor that creates a new list of literals.
+    """
+    def __init__(self):
+        pass
+
+    """
+    Appends the specified Literal to the end of literals list.
+
+    PARAMETERS
+    ----------
+    literal : Literal
+        Element to be appended to the list
+    """
+    def addLiteral(self, literal: Literal):
+        self.__literals.append(literal)
+
+    """
+    Moves the specified literal to the first of literals list.
+
+    PARAMETERS
+    ----------
+    literal : Literal
+        Element to be moved to the first element of the list
+    """
+    def moveFirst(self, literal: Literal):
+        if self.contains(literal):
+            self.__literals.remove(literal)
+            self.__literals.insert(0, literal)
+
+    """
+    Returns the element at the specified position in literals list.
+
+    PARAMETERS
+    ----------
+    index : int
+        index of the element to return
+        
+    RETURNS
+    -------
+    Literal
+        The element at the specified position in the list
+    """
+    def getLiteral(self, indexOrName) -> Literal:
+        if isinstance(indexOrName, int):
+            return self.__literals[indexOrName]
+        elif isinstance(indexOrName, str):
+            for literal in self.__literals:
+                if literal.getName() == indexOrName:
+                    return literal
+        return None
+
+    """
+    Returns size of literals list.
+
+    RETURNS
+    -------
+    int
+        The size of the list
+    """
+    def literalSize(self) -> int:
+        return len(self.__literals)
+
+    """
+    Returns true if literals list contains the specified literal.
+
+    PARAMETERS
+    ----------
+    literal : Literal
+        Element whose presence in the list is to be tested
+        
+    RETURNS
+    -------
+    bool
+        True if the list contains the specified element
+    """
+    def contains(self, literal: Literal) -> bool:
+        return literal in self.__literals
+
+    """
+    Returns True if literals list contains the specified String literal.
+
+    PARAMETERS
+    ----------
+    literalName : str
+        element whose presence in the list is to be tested
+        
+    RETURNS
+    -------
+        True if the list contains the specified element
+    """
+    def containsLiteral(self, literalName: str) -> bool:
+        for literal in self.__literals:
+            if literal.getName() == literalName:
+                return True
+        return False
+
+    """
+    Removes the first occurrence of the specified element from literals list,
+    if it is present. If the list does not contain the element, it stays unchanged.
+
+    PARAMETERS
+    ----------
+    toBeRemoved : Literal
+        Element to be removed from the list, if present
+    """
+    def removeLiteral(self, toBeRemoved: Literal):
+        self.__literals.remove(toBeRemoved)
+
+    """
+    Method to write Synonyms to the specified file in the XML format.
+
+    PARAMETERS
+    ----------
+    outFile 
+        File to write XML files
+    """
+    def saveAsXml(self, outFile):
+        outFile.write("<SYNONYM>")
+        for literal in self.__literals:
+            literal.saveAsXml(outFile)
+        outFile.write("</SYNONYM>")
+
+    """
+    Overridden toString method to print literals.
+
+    RETURNS
+    -------
+    str
+        Concatenated literals
+    """
+    def __str__(self) -> str:
+        result = ""
+        for literal in self.__literals:
+            result = result + literal.getName() + " "
+        return result
