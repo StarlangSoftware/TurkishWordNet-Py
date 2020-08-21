ecac041a09611ab274c26bcdf5e6131b56b1d230
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-08-21 09:02:21 +0300

    Updated wordnet.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-WordNet',
-    version='1.0.8',
+    version='1.0.9',
     packages=['WordNet', 'WordNet.Similarity'],
     url='https://github.com/olcaytaner/TurkishWordNet-Py',
     license='',
