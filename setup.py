a7f1a36a72e050da37b5bce42f65295825b6891e
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-10-24 23:21:38 +0300

    Updated wordnet. Added examples.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-WordNet',
-    version='1.0.15',
+    version='1.0.16',
     packages=['WordNet', 'WordNet.Similarity'],
     url='https://github.com/olcaytaner/TurkishWordNet-Py',
     license='',
