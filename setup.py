a1e444a665046fffb0a23b7666dcfa3c9b887d6e
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:26:27 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-WordNet',
     version='1.0.17',
     packages=['WordNet', 'WordNet.Similarity'],
-    url='https://github.com/olcaytaner/TurkishWordNet-Py',
+    url='https://github.com/StarlangSoftware/TurkishWordNet-Py',
     license='',
-    author='olcay',
-    author_email='olcaytaner@isikun.edu.tr',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish WordNet KeNet',
     install_requires=['NlpToolkit-MorphologicalAnalysis']
 )
